36789344fc579e082b7e20ffd4999988d6952d82
Orvie Salgado <orvie.salgado@udea.edu.co>
2021-07-24 20:06:04 -0500

    fixing base url for public validations

--- src/utils/commons.js
+++ src/utils/commons.js
@@ -1,5 +1,5 @@
 const production = (process.env.NODE_ENV === 'production');
 export const applicationPaths = {
-    application: production ? 'https://semova.alt111.com' :'http://localhost:8080',
+    application: production ? 'https://semova.alt111.dev' :'http://localhost:8080',
     publicResource: '/validate-practices/public/'
 }
